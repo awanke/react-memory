77e97905188665dd9386d94427363769bb2b9a83
jiangyanping <jiangyanping@domob.cn>
2016-03-16 21:56:12 +0800

    dd

---
+++ install-lib.sh
@@ -0,0 +1,9 @@
+# 运行游戏时需要的依赖包
+npm install --save react lodash
+bower install --save bootstrap
+
+# 编译游戏时需要的依赖包
+npm install --save-dev browserify watchify reactify
+
+# 全局命令行工具
+npm install -g browserify watchify http-server
---
+++ run.sh
@@ -0,0 +1,2 @@
+# watchify -v -o build/app.js js/app.js
+http-server -p 8888
